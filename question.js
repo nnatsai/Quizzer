d4619f036765b5b7ce72d7b94cc728b145fd7acf
Natsai Ndebele <nnatsai28@gmail.com>
2017-10-15 07:02:17 -0400

    Initial commit

---
+++ question.js
@@ -0,0 +1,16 @@
+var questions = [{
+	"question": "Insert the first question here?",
+	"option1": "A jffnvjruv vjrnjfnuer jvbrvbru",
+	"option2": "B",
+	"option3": "C",
+	"option4": "D",
+	"answer": "2"
+}, {
+	"question": "Insert the first question here?",
+	"option1": "A",
+	"option2": "B",
+	"option3": "C",
+	"option4": "D",
+	"answer": "2"
+	
+}];
